88483ffb06711583bb18da11009bebd54f891503
Brian Dinsmore <akaronmexico7@gmail.com>
2019-05-02 21:56:53 -0400

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,90 +1,67 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule, Routes } from '@angular/router';
-import { MatMomentDateModule } from '@angular/material-moment-adapter';
-import { MatButtonModule, MatIconModule } from '@angular/material';
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { TranslateModule } from '@ngx-translate/core';
 import 'hammerjs';
 
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { MatButtonModule, MatIconModule } from '@angular/material';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { FuseProgressBarModule, FuseSidebarModule } from '@fuse/components';
 import { FuseModule } from '@fuse/fuse.module';
 import { FuseSharedModule } from '@fuse/shared.module';
-import { FuseProgressBarModule, FuseSidebarModule, FuseThemeOptionsModule } from '@fuse/components';
-
-import { fuseConfig } from 'app/fuse-config';
-
-import { FakeDbService } from 'app/fake-db/fake-db.service';
+import { TranslateModule } from '@ngx-translate/core';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AppComponent } from 'app/app.component';
-import { AppStoreModule } from 'app/store/store.module';
+import { FakeDbService } from 'app/fake-db/fake-db.service';
+import { fuseConfig } from 'app/fuse-config';
 import { LayoutModule } from 'app/layout/layout.module';
 
 const appRoutes: Routes = [
-    {
-        path        : 'apps',
-        loadChildren: './main/apps/apps.module#AppsModule'
-    },
-    {
-        path        : 'pages',
-        loadChildren: './main/pages/pages.module#PagesModule'
-    },
-    {
-        path        : 'ui',
-        loadChildren: './main/ui/ui.module#UIModule'
-    },
-    {
-        path        : 'documentation',
-        loadChildren: './main/documentation/documentation.module#DocumentationModule'
-    },
-    {
-        path        : 'angular-material-elements',
-        loadChildren: './main/angular-material-elements/angular-material-elements.module#AngularMaterialElementsModule'
-    },
-    {
-        path      : '**',
-        redirectTo: 'apps/dashboards/analytics'
-    }
+  {
+    path: 'apps',
+    loadChildren: './main/apps/apps.module#AppsModule'
+  },
+  {
+    path: 'pages',
+    loadChildren: './main/pages/pages.module#PagesModule'
+  },
+  {
+    path: '**',
+    redirectTo: 'apps/dashboards/analytics'
+  }
 ];
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports     : [
-        BrowserModule,
-        BrowserAnimationsModule,
-        HttpClientModule,
-        RouterModule.forRoot(appRoutes),
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
 
-        TranslateModule.forRoot(),
-        InMemoryWebApiModule.forRoot(FakeDbService, {
-            delay             : 0,
-            passThruUnknownUrl: true
-        }),
+    TranslateModule.forRoot(),
+    InMemoryWebApiModule.forRoot(FakeDbService, {
+      delay: 0,
+      passThruUnknownUrl: true
+    }),
 
-        // Material moment date module
-        MatMomentDateModule,
+    // Material moment date module
+    MatMomentDateModule,
 
-        // Material
-        MatButtonModule,
-        MatIconModule,
+    // Material
+    MatButtonModule,
+    MatIconModule,
 
-        // Fuse modules
-        FuseModule.forRoot(fuseConfig),
-        FuseProgressBarModule,
-        FuseSharedModule,
-        FuseSidebarModule,
-        FuseThemeOptionsModule,
+    // Fuse modules
+    FuseModule.forRoot(fuseConfig),
+    FuseProgressBarModule,
+    FuseSharedModule,
+    FuseSidebarModule,
 
-        // App modules
-        LayoutModule,
-        AppStoreModule
-    ],
-    bootstrap   : [
-        AppComponent
-    ]
+    // App modules
+    LayoutModule
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule
-{
-}
+export class AppModule {}
--- src/app/fake-db/fake-db.service.ts
+++ src/app/fake-db/fake-db.service.ts
@@ -1,101 +1,50 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
-
-import { ProjectDashboardDb } from 'app/fake-db/dashboard-project';
-import { AnalyticsDashboardDb } from 'app/fake-db/dashboard-analytics';
-import { CalendarFakeDb } from 'app/fake-db/calendar';
-import { ECommerceFakeDb } from 'app/fake-db/e-commerce';
-import { AcademyFakeDb } from 'app/fake-db/academy';
-import { MailFakeDb } from 'app/fake-db/mail';
-import { ChatFakeDb } from 'app/fake-db/chat';
-import { FileManagerFakeDb } from 'app/fake-db/file-manager';
 import { ContactsFakeDb } from 'app/fake-db/contacts';
-import { TodoFakeDb } from 'app/fake-db/todo';
-import { ScrumboardFakeDb } from 'app/fake-db/scrumboard';
-import { InvoiceFakeDb } from 'app/fake-db/invoice';
-import { ProfileFakeDb } from 'app/fake-db/profile';
-import { SearchFakeDb } from 'app/fake-db/search';
-import { FaqFakeDb } from 'app/fake-db/faq';
-import { KnowledgeBaseFakeDb } from 'app/fake-db/knowledge-base';
+import { AnalyticsDashboardDb } from 'app/fake-db/dashboard-analytics';
+import { ProjectDashboardDb } from 'app/fake-db/dashboard-project';
+import { FileManagerFakeDb } from 'app/fake-db/file-manager';
 import { IconsFakeDb } from 'app/fake-db/icons';
-import { ChatPanelFakeDb } from 'app/fake-db/chat-panel';
-import { QuickPanelFakeDb } from 'app/fake-db/quick-panel';
+import { MailFakeDb } from 'app/fake-db/mail';
+import { ProfileFakeDb } from 'app/fake-db/profile';
+import { ScrumboardFakeDb } from 'app/fake-db/scrumboard';
+import { TodoFakeDb } from 'app/fake-db/todo';
 
-export class FakeDbService implements InMemoryDbService
-{
-    createDb(): any
-    {
-        return {
-            // Dashboards
-            'project-dashboard-projects' : ProjectDashboardDb.projects,
-            'project-dashboard-widgets'  : ProjectDashboardDb.widgets,
-            'analytics-dashboard-widgets': AnalyticsDashboardDb.widgets,
+export class FakeDbService implements InMemoryDbService {
+  createDb(): any {
+    return {
+      // Dashboards
+      'project-dashboard-projects': ProjectDashboardDb.projects,
+      'project-dashboard-widgets': ProjectDashboardDb.widgets,
+      'analytics-dashboard-widgets': AnalyticsDashboardDb.widgets,
 
-            // Calendar
-            'calendar': CalendarFakeDb.data,
+      // Mail
+      'mail-mails': MailFakeDb.mails,
+      'mail-folders': MailFakeDb.folders,
+      'mail-filters': MailFakeDb.filters,
+      'mail-labels': MailFakeDb.labels,
 
-            // E-Commerce
-            'e-commerce-products' : ECommerceFakeDb.products,
-            'e-commerce-orders'   : ECommerceFakeDb.orders,
+      // File Manager
+      'file-manager': FileManagerFakeDb.files,
 
-            // Academy
-            'academy-categories': AcademyFakeDb.categories,
-            'academy-courses'   : AcademyFakeDb.courses,
-            'academy-course'    : AcademyFakeDb.course,
+      // Contacts
+      'contacts-contacts': ContactsFakeDb.contacts,
+      'contacts-user': ContactsFakeDb.user,
 
-            // Mail
-            'mail-mails'  : MailFakeDb.mails,
-            'mail-folders': MailFakeDb.folders,
-            'mail-filters': MailFakeDb.filters,
-            'mail-labels' : MailFakeDb.labels,
+      // Todo
+      'todo-todos': TodoFakeDb.todos,
+      'todo-filters': TodoFakeDb.filters,
+      'todo-tags': TodoFakeDb.tags,
 
-            // Chat
-            'chat-contacts': ChatFakeDb.contacts,
-            'chat-chats'   : ChatFakeDb.chats,
-            'chat-user'    : ChatFakeDb.user,
+      // Scrumboard
+      'scrumboard-boards': ScrumboardFakeDb.boards,
 
-            // File Manager
-            'file-manager': FileManagerFakeDb.files,
+      // Profile
+      'profile-timeline': ProfileFakeDb.timeline,
+      'profile-photos-videos': ProfileFakeDb.photosVideos,
+      'profile-about': ProfileFakeDb.about,
 
-            // Contacts
-            'contacts-contacts': ContactsFakeDb.contacts,
-            'contacts-user'    : ContactsFakeDb.user,
-
-            // Todo
-            'todo-todos'  : TodoFakeDb.todos,
-            'todo-filters': TodoFakeDb.filters,
-            'todo-tags'   : TodoFakeDb.tags,
-
-            // Scrumboard
-            'scrumboard-boards': ScrumboardFakeDb.boards,
-
-            // Invoice
-            'invoice': InvoiceFakeDb.invoice,
-
-            // Profile
-            'profile-timeline'     : ProfileFakeDb.timeline,
-            'profile-photos-videos': ProfileFakeDb.photosVideos,
-            'profile-about'        : ProfileFakeDb.about,
-
-            // Search
-            'search': SearchFakeDb.search,
-
-            // FAQ
-            'faq': FaqFakeDb.data,
-
-            // Knowledge base
-            'knowledge-base': KnowledgeBaseFakeDb.data,
-
-            // Icons
-            'icons': IconsFakeDb.icons,
-
-            // Chat Panel
-            'chat-panel-contacts' : ChatPanelFakeDb.contacts,
-            'chat-panel-chats': ChatPanelFakeDb.chats,
-            'chat-panel-user': ChatPanelFakeDb.user,
-
-            // Quick Panel
-            'quick-panel-notes' : QuickPanelFakeDb.notes,
-            'quick-panel-events': QuickPanelFakeDb.events
-        };
-    }
+      // Icons
+      icons: IconsFakeDb.icons
+    };
+  }
 }

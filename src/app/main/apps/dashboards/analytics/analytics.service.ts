cb17d59bfe5e37ab52bddc79f620cc9b552e8ca7
user <user@mail.com>
2019-05-23 12:26:10 -0400

    metrics

--- src/app/main/apps/dashboards/analytics/analytics.service.ts
+++ src/app/main/apps/dashboards/analytics/analytics.service.ts
@@ -1,59 +1,67 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  Resolve,
+  RouterStateSnapshot
+} from '@angular/router';
 import { Observable } from 'rxjs';
+import { environment } from 'environments/environment';
 
 @Injectable()
-export class AnalyticsDashboardService implements Resolve<any>
-{
-    widgets: any[];
+export class AnalyticsDashboardService implements Resolve<any> {
+  widgets: any[];
+  keywordMetrics: any[];
+  baseURL = environment.baseUrl;
+  /**
+   * Constructor
+   *
+   * @param {HttpClient} _httpClient
+   */
+  constructor(private _httpClient: HttpClient) {}
 
-    /**
-     * Constructor
-     *
-     * @param {HttpClient} _httpClient
-     */
-    constructor(
-        private _httpClient: HttpClient
-    )
-    {
-    }
+  /**
+   * Resolver
+   *
+   * @param {ActivatedRouteSnapshot} route
+   * @param {RouterStateSnapshot} state
+   * @returns {Observable<any> | Promise<any> | any}
+   */
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<any> | Promise<any> | any {
+    return new Promise((resolve, reject) => {
+      Promise.all([this.getWidgets(), this.getKeywordMetrics()]).then(() => {
+        resolve();
+      }, reject);
+    });
+  }
 
-    /**
-     * Resolver
-     *
-     * @param {ActivatedRouteSnapshot} route
-     * @param {RouterStateSnapshot} state
-     * @returns {Observable<any> | Promise<any> | any}
-     */
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> | Promise<any> | any
-    {
-        return new Promise((resolve, reject) => {
+  /**
+   * Get widgets
+   *
+   * @returns {Promise<any>}
+   */
+  getWidgets(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this._httpClient
+        .get('api/analytics-dashboard-widgets')
+        .subscribe((response: any) => {
+          this.widgets = response;
+          resolve(response);
+        }, reject);
+    });
+  }
 
-            Promise.all([
-                this.getWidgets()
-            ]).then(
-                () => {
-                    resolve();
-                },
-                reject
-            );
-        });
-    }
-
-    /**
-     * Get widgets
-     *
-     * @returns {Promise<any>}
-     */
-    getWidgets(): Promise<any>
-    {
-        return new Promise((resolve, reject) => {
-            this._httpClient.get('api/analytics-dashboard-widgets')
-                .subscribe((response: any) => {
-                    this.widgets = response;
-                    resolve(response);
-                }, reject);
-        });
-    }
+  getKeywordMetrics(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this._httpClient
+        .get(this.baseURL + '/keywordmetrics')
+        .subscribe((response: any) => {
+          this.keywordMetrics = response;
+          resolve(response);
+        }, reject);
+    });
+  }
 }
